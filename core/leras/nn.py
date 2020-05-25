58b85886f700450e14b99a92932b5fafc8eb719f
bilei <bilei@jd.com>
2020-05-25 11:36:15 +0800

    use TF v1.0 method

--- core/leras/nn.py
+++ core/leras/nn.py
@@ -98,7 +98,8 @@ class nn():
                 config = tf.ConfigProto(device_count={'GPU': 0})
             else:
                 nn.tf_default_device = "/GPU:0"
-                config = tf.ConfigProto()
+                #config = tf.ConfigProto()
+                config=tf.compat.v1.ConfigProto()
                 config.gpu_options.visible_device_list = ','.join([str(device.index) for device in device_config.devices])
 
             config.gpu_options.force_gpu_compatible = True
